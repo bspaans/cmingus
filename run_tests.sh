c532fc447a18e213cbef0a26f553d01b6a810232
bspaans <onderstekop@gmail.com>
2009-02-09 06:15:12 +0100

    Compiling midi library and unit test program.

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,24 @@ tests_run=$?
 
 if [ $tests_run != "134" ]; then
 	./src/mingus_containers $tests_run
+	tests_run=$?
 else
 	echo "Not passing all tests in core."
+	exit 1
+fi;
+
+
+if [ $tests_run != "134" ]; then
+	./src/mingus_midi $tests_run
+	tests_run=$?
+else
+	echo "Not passing all tests in containers."
+	exit 1
+fi;
+
+
+if [ $tests_run = "134" ]; then
+	echo "Not passing all tests in containers."
+	exit 1
 fi;
 
---
+++ src/midi/fluidsynth.cpp
@@ -0,0 +1,1 @@
+#include "../containers/containers.h"
