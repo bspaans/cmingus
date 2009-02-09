fa3aa1350c6fd7791d6c6b2657d7fca0756f5397
bspaans <onderstekop@gmail.com>
2009-02-09 05:54:11 +0100

    Forgot to add Track files.

---
+++ src/containers/Track.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include "containers.h"
+
+Track::Track()
+{
+	//name = "Untitled";
+}
+
+
+void
+Track::empty()
+{
+	bars.clear();
+}
---
+++ src/containers/Track.h
@@ -0,0 +1,17 @@
+class Track
+{
+
+	public:
+		// Constructors
+		Track();
+
+		// Attributes
+		std::vector<Bar> bars;
+		//string name;
+
+		// Instrument; not implemented yet.
+		
+		// Methods
+		void empty(void);
+
+};
