175fcd718633d686a826ba3f5e46e336f716ef9e
bspaans <onderstekop@gmail.com>
2009-02-16 19:20:43 +0100

    Updated Makefile.am's and configure.ac to use libtool. Now building shared library libmingus.

---
+++ mingw.sh
@@ -0,0 +1,12 @@
+export CC=i586-mingw32msvc-gcc
+export CXX=i586-mingw32msvc-g++
+export LD=i586-mingw32msvc-ld
+export AR=i586-mingw32msvc-ar
+export AS=i586-mingw32msvc-as
+export NM=i586-mingw32msvc-nm
+export STRIP=i586-mingw32msvc-strip
+export RANLIB=i586-mingw32msvc-ranlib
+export DLLTOOL=i586-mingw32msvc-dlltool
+export OBJDUMP=i586-mingw32msvc-objdump
+export RC=i586-mingw32msvc-windres
+./configure CFLAGS="-O3" LDFLAGS="-L/home/bspaans/coding/C/cmingus/" CPPFLAGS="-I/home/bspaans/workspace/mingw-fluidsynth/fluidsynth-1.0.8/include/" CXXFLAGS="-O3"
